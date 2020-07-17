12e03128c22175dfdafc240a2ef04443f2ab5f77
Melissa Ahlstrom <melissa.ahlstrom@hotmail.com>
2020-07-16 23:25:00 -0400

    MQ fixed, AOS animations added, burger nav styles

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-function show() {
-  const nav = document.getElementById('#nav-toggle');
-  nav.classList.add('.show-nav');
-}
+// when MQ is reached, menu is closed aka false (because we set it originally to visbility: hidden to the user has to click to burg to open it)
+let toggleNavStatus = false;
 
+
+let toggleNav = function() {
+  let getMenu = document.querySelector('.mobile-menu');
+  let getMobileNav = document.querySelector('.mobile-nav');
+  
+  if (toggleNavStatus === false) {
+    getMenu.style.visibility = 'visible';
+  }
+  else {
+    getMenu.style.visibility ='hidden';
+  }
+}
